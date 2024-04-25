7ca97d4ce42675dc19c3c8e2fe2e99a35c289791
luciferinfery <rudfedor@gmail.com>
2024-04-25 15:15:24 +0300

    HW_1

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from './Card.jsx';
+
+function App() {
+    return (
+        <div className="App">
+            <Card />
+        </div>
+    );
+}
+export default App;
