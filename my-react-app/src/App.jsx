3815096a01b191ec5ffc532897198c51408310e4
luciferinfery <rudfedor@gmail.com>
2024-04-26 16:15:48 +0300

    HW_2

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import Card from './Card.jsx';
+import ReactDOM from 'react-dom';
+import Card from './Card';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <Card />
+        <div>
+            <h1>Мій додаток React</h1>
+            <h2>Передача только title:</h2>
+            <Card title="hi" />
+            <h2>Передача только text:</h2>
+            <Card text="how are you?" />
+            <h2>Передача и title, и text:</h2>
+            <Card title="hi" text="how are you?" />
         </div>
     );
 }
+
 export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
